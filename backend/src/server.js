4658c40d483fc9af0026c32439247dd43ace5eb7
Dimitris Nimas <hello@dimitrisnimas.gr>
2026-01-05 05:42:52 +0200

    Add Custom Domain

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,8 @@ app.use(cors({
             'http://localhost:5173',
             'http://localhost:4173',
             process.env.FRONTEND_URL,
-            'https://kubikmembersmanagement.netlify.app'
+            'https://kubikmembersmanagement.netlify.app',
+            'https"//members.kubik.gr'
         ].filter(Boolean);
 
         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
